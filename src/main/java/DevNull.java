c96e7f4baa6bcd0811ccb6ac134cfa220c1a63aa
chxei <chxei5667@gmail.com>
2020-04-02 23:54:25 +0400

    init დუბლი 2

---
+++ src/main/java/DevNull.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.JDABuilder;
+
+public class DevNull {
+    public static void main(String[] args) throws Exception {
+        //init jdabuilder
+        JDABuilder jda = new JDABuilder();
+        jda.setToken("NjUyMjI0NTk1NDAyODE3NTQ2.XoZAUA.nZSdqJM185rzRCtj9rHPjjJjxU0");
+        /*
+        Set your config here
+        */
+        jda.build();
+    }
+}
