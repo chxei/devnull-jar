40f316f7211954b4f7f2d24ef995824ae9841ecf
chxei <chxei5667@gmail.com>
2020-04-03 19:20:04 +0400

    feat: heroku preparations
    Added shadow library and fatjar task which creates jar file under /build/libs/

--- src/main/java/DevNull.java
+++ src/main/java/DevNull.java
@@ -3,11 +3,12 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 
+
+//This is where things start
 public class DevNull {
     public static void main(String[] args) throws Exception {
-        //init jdabuilder
         JDABuilder jda = new JDABuilder();
-        jda.setToken("NjUyMjI0NTk1NDAyODE3NTQ2.XoZJVg.Mgh02OZGs4CkAGgnnULYJcP9_40");
+        jda.setToken("NjUyMjI0NTk1NDAyODE3NTQ2.XodSKQ.4lTlqUIvcjXeaL4XHnV9-jhtzdQ");
 
         jda.setActivity(Activity.watching("Luke Smith"));
 
