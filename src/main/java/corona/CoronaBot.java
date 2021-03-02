0db0768370cc6cb0fdbf8da17ae5d6690395557b
chxei <davitchkheidze@outlook.com>
2021-03-02 17:10:09 +0400

    more sonarqube fixes

--- src/main/java/corona/CoronaBot.java
+++ src/main/java/corona/CoronaBot.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 
 import static corona.CoronaDataType.COUNTRYWIDE;
 import static corona.CoronaDataType.WORLDWIDE;
--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -3,21 +3,21 @@ package quotes;
 import java.util.Date;
 
 public class Quote {
-    String quote;
+    String quoteText;
     String author;
     String uploaderUserName;
     String uploaderUserId;
     Date uploadDate;
     Boolean active;
 
-    public Quote(String quote, String author) {
-        this.quote = quote;
+    public Quote(String quoteText, String author) {
+        this.quoteText = quoteText;
         this.author = author;
     }
     @Override
     public String toString() {
         String newLine = System.getProperty("line.separator");
-        return "> **"+this.quote+"**"+newLine
+        return "> **"+this.quoteText+"**"+newLine
                 + "__*"+this.author+"*__";
     }
 
